9d87e6182d5ef2c0867e29642806f4517297cc56
DungDT <dungdt@rikkeisoft.com>
2016-03-30 09:57:52 +0700

    lesson 14: auto suggest dropdown search

--- backend/views/branches/_form.php
+++ backend/views/branches/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use backend\models\Companies;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Branches */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,12 +13,18 @@ use yii\helpers\ArrayHelper;
 <div class="branches-form">
 
     <?php $form = ActiveForm::begin(); ?>
-  
-    <?= $form->field($model, 'companies_company_id')->dropDownList(
-            ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
-            ['prompt' => 'Select Company']
-    )?>
 
+    <?=
+        $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
+            'data' => ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
+            'language' => 'en',
+            'options' => ['placeholder' => 'Select a state ...'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]);
+    ?>
+  
     <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
