4e24fa0ea1803ecf946596dc51bddaeae37eb17b
DungDT <dungdt@rikkeisoft.com>
2016-03-31 11:15:02 +0700

    Lesson 30: Karthik Gridview expand row

--- backend/views/po/index.php
+++ backend/views/po/index.php
@@ -1,7 +1,8 @@
 <?php
 
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use backend\models\PoItemSearch;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\PoSearch */
@@ -21,9 +22,25 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'export' => false,
         'filterModel' => $searchModel,
         'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+            [
+                'class' => 'kartik\grid\ExpandRowColumn',
+                'value' => function($model, $key, $index, $column) {
+                    return GridView::ROW_COLLAPSED;
+                },
+                'detail' => function($model, $key, $index, $column) {
+                    $searchModel = new PoItemSearch();
+                    $searchModel->po_id = $model->id;
+                    $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+                    
+                    return Yii::$app->controller->renderPartial('_poitems', [
+                        'searchModel' => $searchModel,
+                        'dataProvider' => $dataProvider
+                    ]);
+                },
+            ],
 
             'id',
             'po_no',
