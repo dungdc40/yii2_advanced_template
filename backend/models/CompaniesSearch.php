23e24c7e398b3e2655de4f374facb142a98693ed
DungDT <dungdt@rikkeisoft.com>
2016-03-31 14:11:27 +0700

    Lesson 32: global search

--- backend/models/CompaniesSearch.php
+++ backend/models/CompaniesSearch.php
@@ -12,6 +12,7 @@ use backend\models\Companies;
  */
 class CompaniesSearch extends Companies
 {
+    public $globalSearch;
     /**
      * @inheritdoc
      */
@@ -19,7 +20,7 @@ class CompaniesSearch extends Companies
     {
         return [
             [['company_id'], 'integer'],
-            [['company_name', 'company_email', 'company_address', 'company_created_date', 'company_status'], 'safe'],
+            [['company_name','globalSearch', 'company_email', 'company_address', 'company_created_date', 'company_status'], 'safe'],
         ];
     }
 
@@ -55,15 +56,10 @@ class CompaniesSearch extends Companies
             return $dataProvider;
         }
 
-        $query->andFilterWhere([
-            'company_id' => $this->company_id,
-            'company_created_date' => $this->company_created_date,
-        ]);
-
-        $query->andFilterWhere(['like', 'company_name', $this->company_name])
-            ->andFilterWhere(['like', 'company_email', $this->company_email])
-            ->andFilterWhere(['like', 'company_address', $this->company_address])
-            ->andFilterWhere(['like', 'company_status', $this->company_status]);
+        $query->orFilterWhere(['like', 'company_name', $this->globalSearch])
+            ->orFilterWhere(['like', 'company_email', $this->globalSearch])
+            ->orFilterWhere(['like', 'company_address', $this->globalSearch])
+            ->orFilterWhere(['like', 'company_status', $this->globalSearch]);
 
         return $dataProvider;
     }
