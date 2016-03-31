508ea055eb99e7413b72454c67db632057760e45
DungDT <dungdt@rikkeisoft.com>
2016-03-31 16:19:43 +0700

    Leson 34: full calendar part 2/2

--- backend/web/js/main.js
+++ backend/web/js/main.js
@@ -1,4 +1,11 @@
 $(function(){
+  $(document).on('click', '.fc-day', function(){
+    var date = $(this).attr('data-date');  
+    $.get('index.php?r=events/create', {'date':date}, function(data){
+      $('#modal').modal('show').find('#modalContent').html(data);
+    });
+    
+  });
   $('#modalButton').click(function(){
     $('#modal').modal('show').find('#modalContent').load($(this).attr('value'));
   });
