994f3a3212a07653b8cb799bf367a8b01133f769
Dzung Do <kiemt95@gmail.com>
2016-04-01 14:59:38 +0700

    Lesson 49: setting and getting cookies

--- backend/controllers/BranchesController.php
+++ backend/controllers/BranchesController.php
@@ -212,4 +212,28 @@ class BranchesController extends Controller
         }
         die('okay');
     }
+    
+    //Lesson 48
+    public function actionUpload()
+    {
+        $fileName = 'file';
+        $uploadPath = 'uploads';
+
+        if (isset($_FILES[$fileName])) {
+            $file = \yii\web\UploadedFile::getInstanceByName($fileName);
+
+            //Print file data
+            //print_r($file);
+
+            if ($file->saveAs($uploadPath . '/' . $file->name)) {
+                //Now save file data to database
+
+                echo \yii\helpers\Json::encode($file);
+            }
+        } else {
+            return $this->render('upload');
+        }
+
+        return false;
+    }
 }
---
+++ backend/views/branches/upload.php
@@ -0,0 +1,14 @@
+<?php
+echo \kato\DropZone::widget([
+       'options' => [
+           'url' => 'index.php?r=branches/upload',
+           'maxFilesize' => '2',
+       ],
+       'clientEvents' => [
+           'complete' => "function(file){console.log(file)}",
+           'removedfile' => "function(file){alert(file.name + ' is removed')}"
+       ],
+   ]);
+
+?>
+
